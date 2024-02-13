920ddf4e4b75465f30746efc160c253c5f88e919
Manitkumartomar <manitkumar8979@gmail.com>
2024-02-13 14:55:54 +0530

    Task-2, Fibbonacci series upto a specified no. of terms

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,22 +1,34 @@
 public class FibonacciSeries {
-  static int n1=0,n2=1,n3=0;
-  public static void printFibo(int count){
-    if (count>0) {
-        n3=n1+n2;
-        n1=n2;
-        n2=n3;
-        System.out.print(" "+n3);
-        printFibo(count-1);
+  // Static variables to store the initial Fibonacci sequence values
+  static int n1 = 0, n2 = 1, n3 = 0;
+
+  // Method to print the Fibonacci series using recursion
+  public static void printFibo(int count) {
+    // Base case: If count is greater than zero
+    if (count > 0) {
+      // Calculate the next Fibonacci number
+      n3 = n1 + n2;
+      n1 = n2;
+      n2 = n3;
+
+      // Print the next Fibonacci number
+      System.out.print(" " + n3);
+
+      // Reucrsively call printFibo method with count decremented by 1
+      printFibo(count - 1);
     }
   }
-    
-    public static void main(String[] args) {
-        // Fibonacci series using recursion
-        int count = 10;
-        System.out.print(n1+" "+n2);
-        printFibo(count-2);
-        
-        }
 
-    }
+  // Main method to execute the program
+  public static void main(String[] args) {
+    // Number of Fibonacci numbers to print
+    int count = 10;
 
+    // Print the initial Fibonacci numbers
+    System.out.print(n1 + " " + n2);
+    // Call the printFibo method to print the remaining Fibonacci numbers
+    printFibo(count - 2);// Subtracting 2 as the first two numbers are already printed
+
+  }
+
+}
